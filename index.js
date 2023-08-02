ce7b5a60511ad3287d555dc0dce8c984d011921f
priyam <priyamshankar.5@gmail.com>
2023-08-02 15:53:37 +0530

    cors

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ require("./Controller/emailScheduler");
 
 // Enable CORS for all routes
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     next();
